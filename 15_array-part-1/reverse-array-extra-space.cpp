fcfb7c59f8665799ad08a1c4f0e0122b03d5e97b
bikundkumar <kumarbikund2017@gmail.com>
2024-10-02 12:35:27 +0530

    a

--- 15_array-part-1/reverse-array-extra-space.cpp
+++ 15_array-part-1/reverse-array-extra-space.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void printArr(int arr[], int lenth) {
+void printArr(int arr[], int length) {
 
-    for (int i = 0; i < lenth; i++) {
+    for (int i = 0; i < length; i++) {
         cout << arr[i] << endl;
     }
 }
 
 int main() {
     int arr[] = {5,4,3,9,2};
-    int lenth = sizeof(arr) / sizeof(int);
+    int length = sizeof(arr) / sizeof(int);
 
-    int copyArr[lenth];
+    int copyArr[length];
 
-    for (int i = 0; i < lenth; i++) {
-        int j = lenth-i-1;
+    for (int i = 0; i < length; i++) {
+        int j = length-i-1;
         copyArr[i] = arr[j]; // {2,9,3,4,5}
                        // index  0,1,2,3,4   
     }
 
-    for(int i = 0; i < lenth; i++) {
+    for(int i = 0; i < length; i++) {
         arr[i] = copyArr[i];               
     }
 
-    printArr(arr,lenth);
+    printArr(arr,length);
 
     return 0;
 }
