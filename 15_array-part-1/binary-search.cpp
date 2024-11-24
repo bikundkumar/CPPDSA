cf1860a386307bab4d6bfb777a93cc015a9e7a6c
bikundkumar <kumarbikund2017@gmail.com>
2024-11-24 22:40:17 +0530

    file change

--- 15_array-part-1/binary-search.cpp
+++ 15_array-part-1/binary-search.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int binSearch(int arr[], int n, int key) {
     int start = 0, end = n - 1;
 
-    while(start <= n) {
+    while(start < n) {
         int mid = (start + end) /2;
 
         if(arr[mid] == key) {
