7fce04f86e70ee4ce791e190f0ddb972701420f4
bikundkumar <kumarbikund2017@gmail.com>
2024-11-24 17:10:33 +0530

    a

--- 15_array-part-1/linear-search.cpp
+++ 15_array-part-1/linear-search.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int linear_search(int arr[], int n, int key) {
-    for (int i = 0; i < n; i++) {
+int search_linear(int arr[], int n, int key) {
+    for(int i = 0; i < n; i++) {
         if(arr[i] == key) {
             return i;
         }
@@ -11,41 +11,9 @@ int linear_search(int arr[], int n, int key) {
 }
 
 int main() {
-    int arr[] = {4,6,3,8,4,83,8,3,833,7,3,85,2,75,37,4};
+    int arr[] = {4,3,8,5,9};
     int n = sizeof(arr) / sizeof(int);
-    int key = 75;
+    int key = 8;
 
-    cout << linear_search(arr,n,key) << endl;
-
-    return 0;
+    cout << search_linear(arr,n,key) << endl;
 }
-
-// 2nd Program
-
-// #include <iostream>
-// using namespace std;
-
-// int linear_search(int arr[], int n, int key) {
-//     for (int i = 0; i < n; i++) {
-//         if (arr[i] == key) {
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
-
-// int main() {
-//     int arr[] = {4, 6, 3, 8, 4, 83, 8, 3, 833, 7, 3, 85, 2, 75, 37, 4};
-//     int n = sizeof(arr) / sizeof(int);
-//     int key = 37;
-
-//     int index = linear_search(arr, n, key);
-
-//     if (index != -1) {
-//         cout << "Value is " << arr[index] << " at index: " << index << endl;
-//     } else {
-//         cout << "Key not found!" << endl;
-//     }
-
-//     return 0;
-// }
