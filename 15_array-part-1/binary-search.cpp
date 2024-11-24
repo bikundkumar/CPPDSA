8c7f4d82985c8e7b74f5cea00dfddf6f67dfec1c
bikundkumar <kumarbikund2017@gmail.com>
2024-11-24 22:26:43 +0530

    a

---
+++ 15_array-part-1/binary-search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int binSearch(int arr[], int n, int key) {
+    int start = 0, end = n - 1;
+
+    while(start <<= n) {
+        int mid = (start + end) /2;
+
+        if(arr[mid] == key) {
+            return mid;
+        }
+        else if (arr[mid] < key) {
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+int main() {
+    int arr[] = {4,9,10,20,40};
+    int n = sizeof(arr) / sizeof(int);
+    int key = 10;
+
+    cout << binSearch(arr,n,key) << endl;
+
+    return 0;
+}
