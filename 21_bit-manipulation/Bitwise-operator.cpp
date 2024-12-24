387a3bc6682eb88d9ee075310ea6c23007f2ca59
bikundkumar <kumarbikund2017@gmail.com>
2024-12-25 00:31:54 +0530

    add more file

--- 21_bit-manipulation/Bitwise-operator.cpp
+++ 21_bit-manipulation/Bitwise-operator.cpp
@@ -5,6 +5,7 @@ int main() {
     cout << (6 & 10) << endl;  // Bitwise AND &
     cout << (3 | 5) << endl; // Bitwise OR |
     cout << (3 ^ 5) << endl; // Bitwise XOR ^
+    cout << (~12) << endl;  // Bitwise NOT ~
 
     return 0;
 }
