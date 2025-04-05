b8a10d9311f5620c439b97e2b6c7a8e6fe78ba28
“bikundkumar” <kumarbikund2017@gmail.com>
2025-04-05 23:49:39 +0530

    add new 1 files

--- Arrays/easy/check_if_an_array_is_sorted.cpp
+++ Arrays/easy/check_if_an_array_is_sorted.cpp
@@ -1,23 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-bool isSorted(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] < arr[n - 1])
-            return false;
-    }
+bool isSorted(int arr[], int n) {
+  for (int i = 1; i < n; i++) {
+    if (arr[i] < arr[i - 1])
+      return false;
+  }
 
-    return true;
+  return true;
 }
 
-int main()
-{
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(int);
+int main() {
 
-    cout << isSorted(arr, n);
+  int arr[] = {1, 2, 3, 4, 5}, n = 5;
 
-    cout << endl;
+  printf("%s", isSorted(arr, n) ? "True" : "False");
+
+  cout << endl;
+
 }
